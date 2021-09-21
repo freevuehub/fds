f2a0a2eb9621deb9d33bfbbcc33ebeadaa1b8bac
홍성준 <freevue@drx.gg>
2021-09-21 19:43:22 +0900

    [new]: 프로젝트 생성

---
+++  index.d.ts
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export interface CardProps extends React.Props<Card> {}
+
+declare class Card extends React.Component<CardProps, any> {
+}
+
+
+// declare module 'hello-world' {}
+
+// export default HelloWorld
