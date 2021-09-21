281046a18f70cb38b366e33c1a540eb37d164358
홍성준 <freevue@drx.gg>
2021-09-21 20:00:07 +0900

    v0.0.5

---  index.d.ts
+++  index.d.ts
@@ -4,8 +4,3 @@ export interface CardProps extends React.Props<Card> {}
 
 declare class Card extends React.Component<CardProps, any> {
 }
-
-
-// declare module 'hello-world' {}
-
-// export default HelloWorld
