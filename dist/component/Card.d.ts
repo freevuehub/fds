777d2049c801d7e2651cbeaf7224104510524da0
freevuehub <hackish113@gmail.com>
2021-11-04 22:32:17 +0900

    v0.0.18

--- dist/component/Card.d.ts
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-declare const Card: React.FC;
-export default Card;
--- dist/component/Card.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jsx_runtime_1 = require("@emotion/react/jsx-runtime");
-const Card = (props) => {
-    return (jsx_runtime_1.jsx("div", { children: props.children }, void 0));
-};
-exports.default = Card;
-//# sourceMappingURL=Card.js.map
