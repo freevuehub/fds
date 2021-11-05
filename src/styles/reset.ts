6b01dae4d5c156b17f4f1f279e5d4f26784a5b2f
freevuehub <hackish113@gmail.com>
2021-11-05 12:55:58 +0900

    [new]: 글로벌 스타일 준비

---
+++ src/styles/reset.ts
@@ -0,0 +1,15 @@
+import { css } from '@emotion/react'
+
+export default css`
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  ul, ol {
+    list-style: none;
+  }
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`
