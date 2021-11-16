7b814658b68680a6c207fbc78174a44b6863bad6
freevuehub <hackish113@gmail.com>
2021-11-16 22:55:41 +0900

    <build> Rollup 현재까지 작업물 빌드 파일

---
+++ dist/component/NavButton/index.d.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+export interface IProps {
+    onChange?: Function;
+    active?: boolean;
+    className?: string;
+}
+declare const NavButton: React.FC<IProps>;
+export default NavButton;
