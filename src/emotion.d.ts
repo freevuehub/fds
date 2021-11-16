eb141fe4ae7a665bdc71dfd99e00ec0f8dabaea3
freevuehub <hackish113@gmail.com>
2021-11-16 23:00:21 +0900

    <refactor> Type 수정 외부 emotion theme type과 충돌을 방지하기 위한 모듈 타입 export 및 적용

--- src/emotion.d.ts
+++ src/emotion.d.ts
@@ -1,27 +1,6 @@
 import '@emotion/react'
+import { Theme as FDSTheme } from '.'
 
 declare module '@emotion/react' {
-  export interface Theme {
-    mode: 'light' | 'dark'
-    common: {
-      primary: string
-      white: string
-      black: string
-      grey: string
-    }
-    color: {
-      app: string
-      content: string
-      text: string
-      table: {
-        th: string
-        td: string
-        background: string
-        border: string
-      }
-    }
-    style: {
-      shadow: string
-    }
-  }
+  export interface Theme extends FDSTheme {}
 }
