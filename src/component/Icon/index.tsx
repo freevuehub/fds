154e457192e4c6cffeb123fc866739b4d686c014
freevuehub <hackish113@gmail.com>
2021-11-17 18:09:21 +0900

    <improved>: Icon 개선 Icon Component 사이즈 조절 props 추가

--- src/component/Icon/index.tsx
+++ src/component/Icon/index.tsx
@@ -5,15 +5,17 @@ import { FontAwesomeIcon  } from '@fortawesome/react-fontawesome'
 export interface IProps {
   icon: any
   className?: string
+  size?: number
 }
 
-const IconCss = css`
+const IconCss = (props: IProps) => () => css`
   display: block;
   width: 100%;
+  font-size: ${props.size ? `${props.size}px` : 'auto'};
 `
 
 const Icon: React.FC<IProps> = (props) =>(
-  <FontAwesomeIcon css={IconCss} icon={props.icon} className={props.className} />
+  <FontAwesomeIcon css={IconCss(props)} icon={props.icon} className={props.className} />
 )
 
 export default Icon
