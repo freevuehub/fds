5d67e47f5ebcb475fd3f74055397104a57b68755
freevuehub <hackish113@gmail.com>
2021-11-17 18:08:46 +0900

    <style>: 공통 스타일 추가 여백 및 간격 auto 값 추가 overlay 및 relative 추가

--- src/styles/common.ts
+++ src/styles/common.ts
@@ -1,15 +1,24 @@
-import { css } from '@emotion/react'
+import { css, Theme } from '@emotion/react'
 
-const diff = [0, 1, 2, 3, 4, 5]
+const diff = ['auto', 0, 1, 2, 3, 4, 5]
 const itemsRange = ['center', 'flex-start', 'flex-end']
 const contentRange = ['center', 'space-around', 'space-between']
 const directions = ['column', 'row']
 const overflow = ['hidden', 'auto', 'scroll']
 
-export default css`
+export default (theme: Theme) => css`
   .fill {
     &.width { width: 100%; }
     &.height { height: 100%; }
+  }
+  
+  .relative {
+    position: relative;
+    transform: translate(0);
+  }
+  .overlay {
+    background-color: ${theme.color.app}70;
+    backdrop-filter: blur(10px);
   }
 
   .d-flex {
@@ -40,17 +49,32 @@ export default css`
     .ac-${item} { align-content: ${item}; }
   `)}
   
-  ${diff.map((item: number) => `
-    .mt-${item} { margin-top: ${item * 5}px; }
-    .mb-${item} { margin-bottom: ${item * 5}px; }
-    .ml-${item} { margin-left: ${item * 5}px; }
-    .mr-${item} { margin-right: ${item * 5}px; }
-    .ma-${item} { margin: ${item * 5}px; }
-    
-    .pt-${item} { padding-top: ${item * 5}px; }
-    .pb-${item} { padding-bottom: ${item * 5}px; }
-    .pl-${item} { padding-left: ${item * 5}px; }
-    .pr-${item} { padding-right: ${item * 5}px; }
-    .pa-${item} { padding: ${item * 5}px; }
-  `)}
+  ${diff.map((item: number | string) => typeof item === 'number'
+    ? `
+      .mt-${item} { margin-top: ${item * 5}px; }
+      .mb-${item} { margin-bottom: ${item * 5}px; }
+      .ml-${item} { margin-left: ${item * 5}px; }
+      .mr-${item} { margin-right: ${item * 5}px; }
+      .ma-${item} { margin: ${item * 5}px; }
+      
+      .pt-${item} { padding-top: ${item * 5}px; }
+      .pb-${item} { padding-bottom: ${item * 5}px; }
+      .pl-${item} { padding-left: ${item * 5}px; }
+      .pr-${item} { padding-right: ${item * 5}px; }
+      .pa-${item} { padding: ${item * 5}px; }
+    `
+    : `
+      .mt-${item} { margin-top: ${item}; }
+      .mb-${item} { margin-bottom: ${item}; }
+      .ml-${item} { margin-left: ${item}; }
+      .mr-${item} { margin-right: ${item}; }
+      .ma-${item} { margin: ${item}; }
+      
+      .pt-${item} { padding-top: ${item}; }
+      .pb-${item} { padding-bottom: ${item}; }
+      .pl-${item} { padding-left: ${item}; }
+      .pr-${item} { padding-right: ${item}; }
+      .pa-${item} { padding: ${item}; }
+    `
+  )}
 `
