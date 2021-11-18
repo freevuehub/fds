37d330e705c3484d6d710fae71f70bcd2754eb10
freevuehub <hackish113@gmail.com>
2021-11-18 18:25:21 +0900

    <improved> Storybook 스토리북 Doc 파일 개선

--- src/component/Avatar/Avatar.stories.tsx
+++ src/component/Avatar/Avatar.stories.tsx
@@ -1,44 +1,31 @@
 import React from 'react'
 import { Meta, Story } from '@storybook/react/types-6-0'
-import Component, { IProps } from '.'
-import { Divider } from '../'
+import Avatar, { IProps } from '.'
 
 export default {
   title: 'Component/UI/Avatar',
-  component: Component,
-  decorators: [
-    (Story) => (
-      <>
-        <h1 className="mb-5">Avatar</h1>
-        <Divider className="mb-5" />
-        <Story />
-      </>
-    )
-  ],
+  component: Avatar,
   args: {
     src: 'https://cdn.pixabay.com/photo/2019/09/14/09/44/cat-4475583_1280.png'
   }
 } as Meta
 
-const Default: Story<IProps> = (args) => <Component {...args} />
-const Template: Story<IProps> = (args) => (
+export const Default: Story<IProps> = (props) => (
   <>
-    <h3 className="mb-2">Size 180px</h3>
-    <div className="d-flex">
-      <Default size={180} className="mb-5 mr-3" {...args}  />
-      <Default size={180} className="mb-5" tile {...args}  />
-    </div>
-    <h3 className="mb-2">Size 110px (Default)</h3>
-    <div className="d-flex">
-      <Default className="mb-5 mr-3" {...args}  />
-      <Default className="mb-5" tile {...args}  />
-    </div>
-    <h3 className="mb-2">Size 80px</h3>
-    <div className="d-flex">
-      <Default size={80} className="mb-5 mr-3" {...args}  />
-      <Default size={80} className="mb-5" tile {...args}  />
-    </div>
+    <Avatar {...props} />
   </>
 )
 
-export const Avatar = Template.bind({})
+export const Tile: Story<IProps> = (props) => (
+  <>
+    <Avatar {...props} tile />
+  </>
+)
+
+export const Size: Story<IProps> = (props) => (
+  <div className="d-flex ai-center jc-space-around">
+    <Avatar {...props} size={120} />
+    <Avatar {...props} size={80} />
+    <Avatar {...props} size={40} />
+  </div>
+)
--- src/component/Icon/Icon.stories.tsx
+++ src/component/Icon/Icon.stories.tsx
@@ -1,25 +1,53 @@
 import React from 'react'
 import { Meta, Story } from '@storybook/react'
-import Component, { IProps } from '.'
-import { faTimes, faSpinner } from '../../'
+import Icon, { IProps } from '.'
+import { faTimes, faSpinner, faCross, faStar } from '../../'
 
 export default {
   title: 'Component/UI/Icon',
-  component: Component,
-  decorators: [
-    (Story) => (
-      <>
-        <Story />
-      </>
-    )
-  ]
+  component: Icon,
 } as Meta
 
-const Template: Story<IProps> = () => (
+const icons = [faTimes, faSpinner, faCross, faStar]
+
+export const Default: Story<IProps> = () => (
+  <div className="d-flex jc-space-around">
+    {
+      React.Children.toArray(
+        icons.map((icon) => <Icon icon={icon} />)
+      )
+    }
+  </div>
+)
+
+export const Size: Story<IProps> = () => (
   <>
-    <Component icon={faTimes} />
-    <Component icon={faSpinner} />
+    {
+      [100, 60, 20].map((size) => (
+        <div className="d-flex pt-5 pb-5 jc-space-evenly" key={size}>
+          {
+            React.Children.toArray(
+              icons.map((icon) => <Icon size={size} icon={icon} />)
+            )
+          }
+        </div>
+      ))
+    }
   </>
 )
 
-export const Icon = Template.bind({})
+export const Color: Story<IProps> = () => (
+  <>
+    {
+      ['red', 'blue', 'green'].map((color) => (
+        <div className="d-flex pt-5 pb-5 jc-space-evenly" key={color}>
+          {
+            React.Children.toArray(
+              icons.map((icon) => <Icon color={color} icon={icon} />)
+            )
+          }
+        </div>
+      ))
+    }
+  </>
+)
