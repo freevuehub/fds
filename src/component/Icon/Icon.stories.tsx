bd0e94e54c097c1ed82b2266fcbe0c10b0308cc8
freevuehub <hackish113@gmail.com>
2021-12-20 21:19:50 +0900

    [improved] 스토리북 대응 아이콘 변경 부분 대응

--- src/component/Icon/Icon.stories.tsx
+++ src/component/Icon/Icon.stories.tsx
@@ -1,53 +1,63 @@
 import React from 'react'
 import { Meta, Story } from '@storybook/react'
 import Icon, { IProps } from '.'
-import { faTimes, faSpinner, faCross, faStar } from '../../'
+import { faSolid } from '../../'
 
 export default {
   title: 'Component/UI/Icon',
   component: Icon,
 } as Meta
 
-const icons = [faTimes, faSpinner, faCross, faStar]
+export const Default: Story<IProps> = () => {
+  const { faTimes, faSpinner, faCross, faStar } = faSolid
 
-export const Default: Story<IProps> = () => (
-  <div className="d-flex jc-space-around">
-    {
-      React.Children.toArray(
-        icons.map((icon) => <Icon icon={icon} />)
-      )
-    }
-  </div>
-)
+  return (
+    <div className="d-flex jc-space-around">
+      {
+        React.Children.toArray(
+          [faTimes, faSpinner, faCross, faStar].map((icon) => <Icon icon={icon} />)
+        )
+      }
+    </div>
+  )
+}
 
-export const Size: Story<IProps> = () => (
-  <>
-    {
-      [100, 60, 20].map((size) => (
-        <div className="d-flex pt-5 pb-5 jc-space-evenly" key={size}>
-          {
-            React.Children.toArray(
-              icons.map((icon) => <Icon size={size} icon={icon} />)
-            )
-          }
-        </div>
-      ))
-    }
-  </>
-)
+export const Size: Story<IProps> = () => {
+  const { faTimes, faSpinner, faCross, faStar } = faSolid
 
-export const Color: Story<IProps> = () => (
-  <>
-    {
-      ['red', 'blue', 'green'].map((color) => (
-        <div className="d-flex pt-5 pb-5 jc-space-evenly" key={color}>
-          {
-            React.Children.toArray(
-              icons.map((icon) => <Icon color={color} icon={icon} />)
-            )
-          }
-        </div>
-      ))
-    }
-  </>
-)
+  return (
+    <>
+      {
+        [100, 60, 20].map((size) => (
+          <div className="d-flex pt-5 pb-5 jc-space-evenly" key={size}>
+            {
+              React.Children.toArray(
+                [faTimes, faSpinner, faCross, faStar].map((icon) => <Icon size={size} icon={icon} />)
+              )
+            }
+          </div>
+        ))
+      }
+    </>
+  )
+}
+
+export const Color: Story<IProps> = () => {
+  const { faTimes, faSpinner, faCross, faStar } = faSolid
+
+  return (
+    <>
+      {
+        ['red', 'blue', 'green'].map((color) => (
+          <div className="d-flex pt-5 pb-5 jc-space-evenly" key={color}>
+            {
+              React.Children.toArray(
+                [faTimes, faSpinner, faCross, faStar].map((icon) => <Icon color={color} icon={icon} />)
+              )
+            }
+          </div>
+        ))
+      }
+    </>
+  )
+}
