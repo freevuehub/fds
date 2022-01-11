26b8416ab8d877404aaf729ed12e0188fbc2dc8a
freevuehub <hackish113@gmail.com>
2022-01-11 21:55:50 +0900

    빌드파일

---
+++ dist/component/Logo/index.d.ts
@@ -0,0 +1,9 @@
+import React from 'react';
+interface IProps {
+    width?: number;
+    height?: number;
+    background?: string;
+    style?: React.CSSProperties;
+}
+declare const Logo: React.FC<IProps>;
+export default Logo;
